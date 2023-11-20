0a8b37a94a981887c578f51a5743f0cf52d3d05c
Diego Alves Teixeira <diegoteialves@gmail.com>
2023-11-20 15:58:25 -0300

    projeto finalizado

--- app3.py
+++ app3.py
@@ -2,15 +2,31 @@ import math
 
 class Line:
     
-    def __init__(self, coordenadas1, coordenadas2):
-        self.coordenadas1 = coordenadas1
-        self.coordenadas2 = coordenadas2
+    def __init__(self, coordenada1, coordenada2, coordenada3, coordenada4):
+        self.coordenada1 = coordenada1
+        self.coordenada2 = coordenada2
+        self.coordenada3 = coordenada3
+        self.coordenada4 = coordenada4
     
     def distance(self):
-        distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        distance = math.sqrt((self.coordenada2 - self.coordenada1)**2 + (self.coordenada4 - self.coordenada3)**2)
         return distance
 
-coordenada1 = input("Informe a coordenada 1")
-coordenada2 = input("Informe a coordenada 2")
+    def slope(self):
+        if self.coordenada3 - self.coordenada1 != 0:  # Evita divisão por zero
+            slope = (self.coordenada4 - self.coordenada2) / (self.coordenada3 - self.coordenada1)
+            return slope
+        else:
+            return "Indefinido (divisão por zero)"
 
-line = Line(coordenada1, coordenada2)
+coordenada1 = int(input("Informe a coordenada 1: "))
+coordenada2 = int(input("Informe a coordenada 2: "))
+coordenada3 = int(input("Informe a coordenada 3: "))
+coordenada4 = int(input("Informe a coordenada 4: "))
+
+line = Line(coordenada1, coordenada2, coordenada3, coordenada4)
+
+print("A distância entre as duas coordenadas é:", line.distance())
+
+slope_result = line.slope()
+print("O declive entre as duas coordenadas é:", slope_result)
